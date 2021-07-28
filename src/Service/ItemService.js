c1a570d3ace24fde7648c0af9e2a9d6edb770e51
shashika dilhani <shashikadilhani97@gmail.com>
2021-07-28 22:54:02 +0530

    Resolve  Conflicts

--- src/Service/ItemService.js
+++ src/Service/ItemService.js
@@ -1,16 +1,22 @@
 import axios from "axios";
  
 
-const ITEMS_TYPES_API_URL = 'http://localhost:8081/cusotmer/viewAll';
-const ITEMS_WOMEN_TYPES_API_URL = 'http://localhost:8081/item/viewAllTypes';
+const ITEMS_TYPES_API_URL = 'http://localhost:8080/admin/viewAll';
+const ITEMS_CATEGORY_TYPES_API_URL = 'http://localhost:8080/item/view/catogery/types';
 
 class ItemService {
     getItemTypes(){
         return axios.get(ITEMS_TYPES_API_URL);
     }
 
-    getAllWomenItemTypes(){
-        return axios.get(ITEMS_WOMEN_TYPES_API_URL);
+    getAllCAtegofyItemTypes(){
+    
+        // return axios.get(ITEMS_TYPES_API_URL);
+        return axios.get(`http://localhost:8080/item/view/catogery/types`, {  
+            params: {
+                cate: 'WOMEN'
+            }
+        })
     }
 }
 
--- src/item/kids.js
+++ src/item/kids.js
@@ -1,23 +1,14 @@
 import React, {Component } from 'react';
-import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
-// import CssBaseline from '@material-ui/core/CssBaseline';
-// import Grid from '@material-ui/core/Grid';
-// import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-// import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-// import Link from '@material-ui/core/Link';
 import './kids.css';
-import UserService from '../Service/UserService';
+import ItemService from '../Service/ItemService';
 import Aux from '../Auxx';
 
   
@@ -31,44 +22,19 @@ class kids extends Component{
       }
   
       componentDidMount(){       
-        UserService.getUsers().then((response) => {
+        ItemService.getAllCAtegofyItemTypes().then((response) => {
             this.setState({ users : response.data})
         });
       }
 
+      
+
 
   render() {
     return (
-       
-                // <Card className="name">
-                  
-                //   <CardMedia 
-                //     image="https://source.unsplash.com/random"
-                //     title="Image title"
-                //   />
-                //   <CardContent className="test">
-                //     <Typography  gutterBottom variant="h5" component="h2">
-                //      {user.name}
-                //     </Typography>
-                //     <Typography >
-                //       This is a media card. You can use this section to describe the content.
-                //     </Typography>
-                //   </CardContent>
-                //   <CardActions className="action">
-                //     <Button size="small" color="primary">
-                //       View
-                //     </Button>
-                //     <Button size="small" color="primary">
-                //       Edit
-                //     </Button>
-                //   </CardActions>
-                // </Card>            
-
-                <React.Fragment className="fragment">
-            
-                
-                <main>
-                  <Container className="cardGrid" spacing={24}>
+                <React.Fragment className="fragment">  
+                <main classNAme="main">
+                  <Container className="cardGrid" spacing={24} back>
                     {/* End hero unit */}
                     <Grid container spacing={4}>
                       {this.state.users.map((user) => (
@@ -81,7 +47,7 @@ class kids extends Component{
                             />
                             <CardContent className="content"  >
                               <Typography className="topo" gutterBottom variant="h5" component="h2">
-                                {user.name}
+                                {user}
                               </Typography>
                               <Typography className="CardContent">
                                 This is a media card. You can use this section to describe the content.
@@ -100,12 +66,8 @@ class kids extends Component{
                       ))}
                     </Grid>
                   </Container>
-                </main>
-                
-              </React.Fragment>
-          
-    
-             
+                </main>               
+              </React.Fragment>          
     );
   }
 }
