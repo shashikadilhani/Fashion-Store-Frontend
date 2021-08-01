2e07eff982c4c4639face6bb14621d412818a12f
shashika dilhani <shashikadilhani97@gmail.com>
2021-08-01 18:01:09 +0530

    Mens file

--- src/item/men.js
+++ src/item/men.js
@@ -1,54 +1,67 @@
 import React, {Component } from 'react';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+// import './women.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-class men extends Component {
-  render(){
-    return(
-      <React.Fragment className="fragment">  
-                <main classNAme="main">
-                  <Container className="cardGrid" spacing={24} back>
-                    {/* End hero unit */}
-                    <Grid container spacing={4}>
-                       
-                        <Grid   xs={8} sm={8} md={3}>
-                          <Card className="card" >
-                            <CardMedia className="media"
-                            
-                              image="https://source.unsplash.com/random"
-                              title="Image title"
-                            />
-                            <CardContent className="content"  >
-                              <Typography className="topo" gutterBottom variant="h5" component="h2">
-                               kjlkl
-                              </Typography>
-                              <Typography className="CardContent">
-                                This is a media card. You can use this section to describe the content.
-                              </Typography>
-                            </CardContent>
-                            {/* <CardActions className="action">
-                              <Button size="small" color="primary">
-                                View
-                              </Button>
-                              <Button size="small" color="primary">
-                                Edit
-                              </Button>
-                            </CardActions> */}
-                          </Card>
-                        </Grid>
-                      )
-                    </Grid>
-                  </Container>
-                </main>               
-              </React.Fragment>       
-    );
+import { Container, Row, Col } from 'reactstrap';
+import Aux from '../Auxx';
+ 
+  
+class men extends Component{
 
+    constructor(){
+        
+          super();
+          this.state = {
+               
+          }
+      }
+
+      handleClick = () => {
+        console.log('HERE!');
+        // this.context.location.transitionTo('login');
+      };
+ 
+  render() {
+    return (    
+      
+         
+    <Grid className="mengrid" direction='row' container  >
+        
+
+        <Grid className="grid11"  >
+            <p>BLOUSES</p>
+        </Grid> 
+        <Grid className="grid12"  >
+        <p>                                          
+          SAREE
+        </p>
+        </Grid> 
+       
+        <Grid className="grid13" >
+        <p>                                          
+           GAWON
+        </p>
+
+        </Grid>
+        <Grid className="grid14"  >
+            <p>SKIRTS</p>
+
+        </Grid>
+        <Grid className="grid15">
+            <p>OFFICE WARE</p>
+        </Grid>
+        <Grid className="grid16"  >
+            <p>DENIMS</p>
+        </Grid>
+        
+
+    </Grid> 
+          
+    );
   }
 }
 
 export default men;
+
+ 
--- src/item/women.js
+++ src/item/women.js
@@ -6,8 +6,6 @@ import Grid from '@material-ui/core/Grid';
 import { Container, Row, Col } from 'reactstrap';
 import Aux from '../Auxx';
  
-
-
   
 class women extends Component{
 
