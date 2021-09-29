a9531702341bed2fb58eb3c9fb2883b70eee069b
shashika dilhani <shashikadilhani97@gmail.com>
2021-09-29 22:07:09 +0530

    resolve confllicts in contact us

--- src/Contactus/Contact.js
+++ src/Contactus/Contact.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import './Contact.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
 
 class Contact extends Component {
@@ -26,19 +28,28 @@ class Contact extends Component {
 
 
                     <Grid className="con-row-2">
-                    Address
-                    No.58, J-Ela Road , Gampaha
-                    <br></br>
-                    Phone
-                    Reception : +94 79 8888 990
-                    Office : +94 34 227 0456
-                    <br></br>
-                    Email
-                    Office : fashionc@gmail.com
-                    Site : fashionc.com
-                    <br></br>
-                    Social
-
+                          <Row className="footer-row">
+                          <Col lg="3" md="6">
+                              <h5 className="footer-topic">Address</h5>
+                              <p className="footer-des">No.398, Galle Road , Aluthgama</p>
+                          </Col>
+                          <Col lg="3" md="6" >
+                              <h5 className="footer-topic">Phone</h5>
+                              <p className="footer-des">Reception :  +94 79 8888 990 <br />Office :  +94 34 227 0456</p>
+                          </Col>
+                          <Col lg="3" md="6" >
+                              <h5 className="footer-topic">Email</h5>
+                              <p className="footer-des">Office :  <a href="#" className="link">happypaws@gmail.com</a> <br />Site :  <a href="#" className="link">happy.com</a></p>
+                          </Col>
+                          <Col lg="3" md="6">
+                              <h5 className="footer-topic">Social</h5>
+                              <div className="round-social light">
+                                  <a href="#" className="link"><i className="fa fa-facebook-square" aria-hidden="true"><br/></i></a>
+                                  <a href="#" className="link"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+                                  <a href="#" className="link"><i className="fa fa-instagram"aria-hidden="true"></i></a>
+                              </div>
+                          </Col>
+                      </Row>
   
 
                     </Grid>
